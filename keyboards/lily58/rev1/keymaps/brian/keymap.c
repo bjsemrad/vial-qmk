edcf3f9b0c547e9ced2cc6dd9ada08e1b06b2bce
Brian Semrad <bjsemrad@gmail.com>
2024-02-03 15:02:52 -0600

    Setup enter hold for shift

--- keyboards/lily58/rev1/keymaps/brian/keymap.c
+++ keyboards/lily58/rev1/keymaps/brian/keymap.c
@@ -52,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,
   KC_LCTL, KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MINS,  KC_BSLS,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RBRC,
-                    KC_LGUI,  KC_LALT, LOWER, LSFT_T(KC_SPC),   KC_ENT,   RAISE, KC_BSPC,  KC_RGUI
+                    KC_LGUI,  KC_LALT, LOWER, KC_SPC,   LSFT_T(KC_ENT),   RAISE, KC_BSPC,  KC_RGUI
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
